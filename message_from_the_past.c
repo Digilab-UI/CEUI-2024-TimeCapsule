0afb39d8a648ba7cc915869b728330d7a1954e77
Tianrider <hadiwijayachristian7@gmail.com>
2024-11-27 01:25:06 +0700

    Initial Commit

---
+++ message_from_the_past.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("\033[1;34m");
+    printf("Written by The First Batch of TEKKOM KKI UI\n");
+    printf("\033[0m");
+
+    printf("========================================================================================\n");
+
+    printf("Author: CH\n");
+    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
+    printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
+    printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+
+    printf("========================================================================================\n");
+
+    return 0;
+}

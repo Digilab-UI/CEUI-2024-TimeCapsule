c68581715620060942f95a35a722c1ad8e46d741
XVentCrossX <landoakmalkane.muhammad@gmail.com>
2024-11-30 10:17:15 +0700

    Changes to message_from_the_past.c into my liking

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,10 +7,12 @@ int main() {
 
     printf("========================================================================================\n");
 
-    printf("Author: CH\n");
-    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
-    printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
-    printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+    printf("Author: Lando\n\n");
+    printf("Hi Guys! it's been a long way since we've been doing this BAPRO, I do hope you guys are doing well too\n");
+    printf("Whether you guys find this message during graduation or studying abroad.\n");
+    printf("Well, actually I don't know what else to say but, good luck with your journey.\n Maybe you're currently at the lowest, or maybe you're having the best of your life, or maybe you're just doing so so\n");
+    printf("All I wish is for you guys to be well and let's have reunion sometimes\n");
+    printf("\nSincerely Lando\n");
 
     printf("========================================================================================\n");
 

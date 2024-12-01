c4c2f6b72d144b101e60c633ef4f4517d9e0d25d
RaihanHermuhadzib <raihanhzkedua@gmail.com>
2024-12-01 23:40:36 +0700

    First Commit

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,9 +7,9 @@ int main() {
 
     printf("========================================================================================\n");
 
-    printf("Author: CH\n");
-    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
-    printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
+    printf("Author: CRaihan HErmuhadzib\n");
+    printf("Hi Mates! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
+    printf("By now, I hope you've learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
 
     printf("========================================================================================\n");

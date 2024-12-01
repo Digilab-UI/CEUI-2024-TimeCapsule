b306b6d6a2cb35c5eb6c006d2b3b42763b8618a3
ArfaSetyadi <setyadiarfa@gmail.com>
2024-12-01 22:46:11 +0700

    arfa sukses

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -16,3 +16,14 @@ int main() {
 
     return 0;
 }
+
+
+int main(){
+	printf("hello future me");
+	printf("so today is the first semester");
+	printf("i am now graduating, cannot believe its already the end for college");
+	printf("shoutout to my aslab and my friends for supporting me throught the semester and i enjoy every moment we had from teamwork, studying together and etc.");
+	printf("so my goal for the future to become rich for sure :) , and of course after college work at a big company, i think thats all for my goal to become financial freedom ");
+	printf("and i hope every single person in tekkom 24 become succesfull");
+	
+}

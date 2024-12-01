a4d2c6107fe8d270336ffe6e83453db926f2e649
Abednego <p.r.b.abednego.s@gmail.com>
2024-12-01 22:15:22 +0700

    message from Abed

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -11,7 +11,10 @@ int main() {
     printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
-
+    printf("Author: AbednegoSamosir\n"); \
+        printf("To all my friends in TEKKOM KKI UI, how are you doing? hopefully you are doing well, are you satisfied with were you are right now? \nIf so I'm happy for you. If you are not, don't worry, great things are waiting for you ahead.\n");
+    printf("I wish all my TEKKOM KKI UI friends the best of luck\n");
+    printf("And to myself, Good luck in the future, hopefully you are not doing the same mistakes you did in your first semester");
     printf("========================================================================================\n");
 
     return 0;

309f9df209ac11e6c7405a6ae759e72d6cc06fc4
hanifbambang <hanifbambang30@gmail.com>
2024-12-01 16:16:01 +0700

    Time Capsule by Hanif Mulia Nugroho

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -12,6 +12,11 @@ int main() {
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
 
+    printf("Author: Hanif Mulia Nugroho\n");
+    printf("Hi everyone! I hope this message is conveyed well. So how is it? Have you achieved any dreams or have you changed your dreams to something bigger.\n");
+    printf("Hopefully now I have learned a lot, be it soft skills or hard skills.\nI hope I'm done with myself so that I can develop myself to be better and be useful to my family, friends and society.\n");
+    printf("I'm sure you can do all the good things, continue to be successful, good luck!\n");
+
     printf("========================================================================================\n");
 
     return 0;

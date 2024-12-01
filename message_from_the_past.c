17774d9389d201a1362d8890c3de529047ff599b
faelyono <rafaelraditya99@gmail.com>
2024-12-01 19:32:52 +0700

    Time Capsule by Rafael Raditya Setyono

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -11,6 +11,11 @@ int main() {
     printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+    
+    printf("Author: Rafael Raditya Setyono\n");
+    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
+    printf("I really hope things went out the way we want it to be since we first went to college.\nI also hope that everyone in our class has passed all classes and everyone goes abroad with no one left behind and to those who wanted double degree including you,succesfully went abroad and stay in touch with each other\nWhatever happens I hope we all dont lose contact and apreciate ouselves for all the hardwork that we did to get here ");
+    printf("I also wish all the aslab great success, thank you for all the work that u did for us and I hope we can see each other again some time,\nI'm waiting for what's next, and I know you'll do great things. Good luck!\n");
 
     printf("========================================================================================\n");
 

f8d90c81a365d374c54a5616ed8538ecfc95404e
ben <benintyaA@gmai.com>
2024-12-01 23:16:24 +0700

    ben time

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -12,12 +12,9 @@ int main() {
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
 
-    printf("Author: Muhammad Risyad Ali\n");
-    printf("I am determined to become a president who brings great change to the country.\n");
-    printf("I hope my teaching assistants are always blessed with health and happiness.\n");
-    printf("After graduation, I will strive to be a more dutiful child for my family.\n");
-
     printf("========================================================================================\n");
+
+    printf("Hey its 2024 you, I just wanted to remind you of how far youve come and encourage you for whats ahead. \nRight now youre a freshman in Computer Engineering at Universitas Indonesia, laying the foundation for your dream of becoming a robotics engineer. It might feel uncertain with studies, your transfer plans, or where youll be in the next few years but trust me, youre exactly where you need to be. \nKeep pushing through tough moments even when it feels like progress is slow, each step counts. Youve got the drive to reach your goals, whether its transferring to the University of Michigan or breaking into robotics. \nStay curious, stay focused, and never hesitate to ask for help. Youve got this, and I cant wait to see all the incredible things youll achieve. Your 2024 self");
 
     return 0;
 }

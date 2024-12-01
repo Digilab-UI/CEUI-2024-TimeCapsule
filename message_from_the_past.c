c1f3c01124770b792024b7f494bcf7c56c3e4558
Jarrouu <129776094+tooOshiroo0o@users.noreply.github.com>
2024-12-01 09:48:44 +0700

    Add files via upload

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,11 +7,19 @@ int main() {
 
     printf("========================================================================================\n");
 
-    printf("Author: CH\n");
-    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
-    printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
-    printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+    printf("Author: AR [Abidzar Rabbani] \n");
+    printf("From you, several years ago\n");
+    printf("Hi There! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
+    printf("By now, I hope you're learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
+    printf("When you're opening this message, i hope that you've learn many such things, and even meet many peoples on your journey \nthat would be a good partners that marks on your journey story\n");
+    printf("But if not, then that's okay, you're destined to met and made friends with anyone you'll met soon, Never Take Anything for Granted\n");
+    printf("Even though you'll never expect who you'd be met or what you'll do in such next year, im grateful to ourself that we've made a progress\n");
+    printf("Forget the past, man. It's been that long year, u don't need to think about it again. YOu have that dream to accomplish, and dont forget about it.\n");
+    printf("My hope for u, for my upcoming years of myself, from your 2024 person, i hope u would doing good academically and socially.\n");
+    printf("If you're reading at this far, congratulations. But life must go on, so keep going!\n");
 
+    printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+    printf("To you, several years later\n");
     printf("========================================================================================\n");
 
     return 0;

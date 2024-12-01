0e90de346ba359f62b9903051b3c2a18a2a70607
Koecheeeng <koechengez3rd@gmail.com>
2024-12-01 22:50:28 +0700

    Koecheng's Time Capsule

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -12,6 +12,11 @@ int main() {
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
 
+    printf("Koecheng, do you still use that name ?\n");
+    printf("If so, thank you. I hope you're doing well. And i also hope that you still keep all your promises.\n");
+    printf("The name speaks for itself, you always love cats and thats why you used it right ?\n");
+    printf("Keep up the good work, use the name, pet the cats, love the peoples.");
+
     printf("========================================================================================\n");
 
     return 0;

f2240749e40a3201a41a9503e37882ff99989410
leeyoka <yohanaindah21@gmail.com>
2024-12-01 19:59:08 +0700

    hyukasduuump messages

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,11 +7,12 @@ int main() {
 
     printf("========================================================================================\n");
 
-    printf("Author: CH\n");
-    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
-    printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
-    printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
-
+    printf("Author: hyukasdump!! <3\n");
+    printf("Halooo!! Pa kabarrr yohhh ?? How's life rn ?? I hope rn you've already in 80% reaching your dream college life WKWKWK\n");
+    printf("Jujur fyi and if you remember, you wrote this nyambi ngebahas finpro KWKWKW multitask since the start huh.\nAnywayyss if youre having a hard time please remember klo kamu udah bisa sampe buka berarti lu gacooorr pls T_T\n");
+    printf("Aku nangis ngetiknya tapi gabisa krn lagi kerkom huhuhu T_T\n");
+    printf("Intinya lo hebat! lo keren dah sampe ini! proud of you, always! Dont forget to always treat yourself kind or even more everytime you did something really good!\n");
+    printf("Ditunggu ya good newsnya terus terussaan, and i hope pas lu baca ini dah dapet gandengan KWKWKWWK, GBU :p\n");
     printf("========================================================================================\n");
 
     return 0;

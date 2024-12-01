28871359eb9707966b6a0cfc52d876a069efaea7
D3zNt <nicolascps4@gmail.com>
2024-12-01 21:19:26 +0700

    to Me from the present

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -11,7 +11,11 @@ int main() {
     printf("Hello friends, have you been well? well I hope you are! You all are amazing each with wonderful talents\n");
     printf("By now, I hope that life is great and your talents are recognized, I do hope that 100\% of tekkom all graduate\n");
     printf("But realistically that might not be feasible, but still I have hope. well I hope when I see this message in X years \n");
-    printf("I have stayed true to my expectations and not deviate from my expectations thats it for me Peace :)\n");
+    printf("I have stayed true to my expectations\n\n");
+
+    printf("This semester has been quite great, and I hope that you will too, I hope that being called intellegent by your peers won't \nmake you study less like it did for me. I wish that you are more disicplined that the present me and that life is great\n");
+    printf("I hope that I am still apart of the organizatiosn that i currently am, (KMB) and many more!\n\n");
+    printf("Thats its for me!, I will see you Next time (also if your going through a hard time i belive in you, you can do it!)\n");
 
     printf("========================================================================================\n");
 

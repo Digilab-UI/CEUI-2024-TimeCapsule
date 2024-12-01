5b04ab25f68d38f0de8e93153770ca81c4dc719e
saddamtitanio <saddam.titanio25@gmail.com>
2024-12-01 20:48:32 +0700

    personal time capsule

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,10 +7,24 @@ int main() {
 
     printf("========================================================================================\n");
 
-    printf("Author: CH\n");
-    printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
-    printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
-    printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+    printf("Author: Saddam Titanio Darmawan\n");
+    printf("Hi Future me, I hope life is treating you well! :).\n");
+    printf("This year, you decided to move to UI and restart your university life because you are insistent on pursuing your dream major. ");
+    printf("I don't regret that decision because I finally get to learn something I enjoy and feel passionate about almost every day ");
+    printf("(minus the irrelevant courses and modules). However, you won't always have the privilege to make transitional decisions like this ");
+    printf("because life cannot continually be bent to your will or made to conform to your desires. ");
+    printf("So, if in the future you encounter a roadblock or a temporary deviation from whatever your initial goal is again, keep going—");
+    printf("one step at a time, one day at a time. You never know where the new path will take you. Who knows? Maybe it will lead you to ");
+    printf("somewhere more beautiful and profound.\n\n");
+
+    printf("Remember that it is okay to make mistakes sometimes—as long as you acknowledge, rectify, and learn from them. ");
+    printf("At the end of the day, it's your first time living this life. It's unrealistic to expect yourself to always make the right decisions, ");
+    printf("especially since there’s no instruction manual to guide you through every situation you face. ");
+    printf("So don't be too hard on yourself :). And always remember that there are many people around you who will always root for you no matter what.\n\n");
+
+    printf("I don't have any expectations for you, but I hope you can find happiness wherever you are. ");
+    printf("Stay true to who you are and your principles. I know you easily get swayed by your environment, ");
+    printf("so please surround yourself with positive and supportive friends. Good luck and all the best to you in the future!\n");
 
     printf("========================================================================================\n");
 

6fc9e46c780df359380f10fd7a4a5124438a1849
Naufalrafiff <naufalrafif0909@gmail.com>
2024-12-01 23:42:13 +0700

    animal kaiser emulator

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -11,6 +11,8 @@ int main() {
     printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
+    printf("Author : Naufal Rafif Adighama\n");
+    printf("GG bgt pam, selamat udah jadi PO MADK 2026 dan sudah melewati semua rintangan di PI dengan baik!!!\n");
 
     printf("========================================================================================\n");
 

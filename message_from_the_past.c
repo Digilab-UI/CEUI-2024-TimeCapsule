5e133214eda533805bfa6db41d4cdb968e352a36
mossexe <syifasarah1411@gmail.com>
2024-12-01 16:12:11 +0700

    moss update

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -13,6 +13,14 @@ int main() {
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");
 
     printf("========================================================================================\n");
+    
+    printf("Author: Moss\n");
+    printf("Hey everyone! Hope you’re all doing well. How has everyone fared these last few years?\n");
+    printf("Where do you guys plan on taking your next step? Are you nervous? I wish you the best of luck on your journey!\n");
+    printf("How 'bout our sweet aslabs? What cool new things are you guys up to now? I bet you’re all working on some amazing projects and having the time of your lives!\n");
+    printf("I’m excited to see where all of you are headed next! Keep me updated! Stay in contact <3\n");
+
+    printf("========================================================================================\n");
 
     return 0;
 }

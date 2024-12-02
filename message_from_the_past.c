04b58e3b297fc71db0b3cde458cff3234afa007e
Gilardino <naufal.gilardino@gmail.com>
2024-12-02 09:11:56 +0700

    Gilar

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -7,7 +7,7 @@ int main() {
 
     printf("========================================================================================\n");
 
-    printf("Author: CH\n");
+    printf("Author: Gilar\n");
     printf("Hi Guys! I hope this message finds you well. So, where are you now? Have you traveled somewhere exciting or made some new friends?\n");
     printf("By now, I hope you'e learned a lot, and maybe even surprised yourself with how much you've accomplished.\nWhen you look back, just remember that you've worked hard to get here, so don't forget to give yourself credit!\n");
     printf("I'm waiting for what's next, and I know you'll do great things. Good luck!\n");

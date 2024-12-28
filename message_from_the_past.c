97166ccb85fe09e3cf38b1672ffb3fa835cb566e
Christian Hadiwijaya <111947738+Tianrider@users.noreply.github.com>
2024-12-28 17:00:26 +0700

    Update message_from_the_past.c

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -45,5 +45,16 @@ int main() {
 
     printf("Hey its 2024 you, I just wanted to remind you of how far youve come and encourage you for whats ahead. \nRight now youre a freshman in Computer Engineering at Universitas Indonesia, laying the foundation for your dream of becoming a robotics engineer. It might feel uncertain with studies, your transfer plans, or where youll be in the next few years but trust me, youre exactly where you need to be. \nKeep pushing through tough moments even when it feels like progress is slow, each step counts. Youve got the drive to reach your goals, whether its transferring to the University of Michigan or breaking into robotics. \nStay curious, stay focused, and never hesitate to ask for help. Youve got this, and I cant wait to see all the incredible things youll achieve. Your 2024 self");
 
+    printf("Author: NC\n");
+    printf("Hello friends, have you been well? well I hope you are! You all are amazing each with wonderful talents\n");
+    printf("By now, I hope that life is great and your talents are recognized, I do hope that 100\% of tekkom all graduate\n");
+    printf("But realistically that might not be feasible, but still I have hope. well I hope when I see this message in X years \n");
+    printf("I have stayed true to my expectations\n\n");
+
+    printf("This semester has been quite great, and I hope that you will too, I hope that being called intellegent by your peers won't \nmake you study less like it did for me. I wish that you are more disicplined that the present me and that life is great\n");
+    printf("I hope that I am still apart of the organizatiosn that i currently am, (KMB) and many more!\n\n");
+    printf("Thats its for me!, I will see you Next time (also if your going through a hard time i belive in you, you can do it!)\n");
+
+    
     return 0;
 }

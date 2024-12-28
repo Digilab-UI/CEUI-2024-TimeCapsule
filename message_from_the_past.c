89f7f01bea672bf28201d1c54ae3fe9afdcc57a9
Christian Hadiwijaya <111947738+Tianrider@users.noreply.github.com>
2024-12-28 17:04:49 +0700

    Rafael

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -114,7 +114,10 @@ int main() {
     printf(" Because I'm sure we'll find our own pace for it. Be patience. So, where are we now? We made new friends and trying new things that you never heard of?\n");
     printf("\nRemember to always be kind and happy whenever and wherever you are. Stay humble and be a good person, see you Nayla!");
 
-	
+	    printf("Author: Rafael Raditya Setyono\n");
+    printf("Hi Bro! I hope this message finds you well. So,how was life?\nDo you have a girlfriend already?\nHave you traveled somewhere exciting or made some new friends?\nDo you have a girlfriend already?\n");
+    printf("I really hope things went out the way we want it to be since we first went to college.\nI also hope that everyone in our class has passed all classes and everyone goes abroad with no one left behind and to those who wanted double degree including you,succesfully went abroad and stay in touch with each other\nWhatever happens I hope we all dont lose contact and apreciate ouselves for all the hardwork that we did to get here ");
+    printf("I also wish all the aslab great success, I hope by now they all already work somewhere nice and I hope you can see them again some time,\nI hope the futures even brighter, and I know you will do just fine. Good luck and wish you all the best!\n");
     return 0;
 }
 	

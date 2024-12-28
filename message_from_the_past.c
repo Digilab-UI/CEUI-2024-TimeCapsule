08cfa8fe853edd1e14cdf4f77c6d621b03c73e92
Christian Hadiwijaya <111947738+Tianrider@users.noreply.github.com>
2024-12-28 17:02:36 +0700

    Nadira

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -75,5 +75,28 @@ int main() {
     printf("I hope that I am still apart of the organizatiosn that i currently am, (KMB) and many more!\n\n");
     printf("Thats its for me!, I will see you Next time (also if your going through a hard time i belive in you, you can do it!)\n");
 
+    printf("\n========================================================================================\n");
+
+	printf("\033[1;34m");
+    printf("Letter to all my friends, passive class dwellers, \nand of course to all the geniuses helping me getting through the semester and each modules :D\n");
+    printf("\033[0m");
+
+    printf("========================================================================================\n");
+
+    printf("Author: Nadira Fayyaza Aisy\n");
+    printf("To start with, i might just be honest that i have no background with things related to my major whatsoever, basically,\nstarting from 0 here and i wanna say sorry to all the lab assitant and my smart friends if i ever give off this 'slow' \nway of thinking and understanding.");
+    printf(" Bless you all, and i thank you, i promise that im always trying, keeping up with \nthe pace, be better each day even when it becomes suffocating sometimes because all the pressure and expectations \ni put to myself.");
+    printf(" I was hoping that i could follow your footsteps on becoming someone that could help and guide others \nwhile also achieving many great things in life, accomplished prestigious competitions, etc. for myself.\n");
+    printf("\nAs for my friends and peers, thank you for battling through this semester alongside me. \nThank you for providing me a warm environment in this very cold, ambitious, and competitive place.\n");
+    printf("Thank you for staying late night with me working out on mountain of assignments, thank you for \nlearning with me through all our mistakes we've made.\n");
+    printf("\nAs for the passive class dwellers, i hope you guys become a better person, not for me nor others, but yourself. \nTrust me whatever you're doing right now, costs more than yourself.");
+    printf(" I know you're struggling yet still it doesnt \njustify throwing loads to others\n");
+    printf("\nAs for my plans in future exchange, im heading to japan. i love japan, i've been there, and i wanna see myself \nstudying, growing, and trying out many opportunities world has offered me.");
+    printf(" I wanna see myself becoming a software \nengineer, and i'll fight for it through many different ways possible. I'll see all of you again in the future, \nand hopefully,");
+    printf(" we've become the person we've always dream to be\n");
+
+    printf("\nPrayers and best wishes for you all \n-N");
+
+
     return 0;
 }
